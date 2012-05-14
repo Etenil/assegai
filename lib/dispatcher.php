1db9950272b5dc60f40457edb503cbe747f17cb5
Etenil <boss@etenil.net>
2012-05-14 22:16:05 +0100

    Written a good portion of the dispatcher. A lot of work still to be done.

--- lib/dispatcher.php
+++ lib/dispatcher.php
@@ -27,6 +27,11 @@ namespace assegai;
 class Dispatcher
 {
     protected $root_path;
+	protected $apps_path;
+	protected $apps;
+
+	protected $apps_routes;
+	protected $routes;
 
     function __construct($root)
     {
@@ -40,8 +45,72 @@ class Dispatcher
      */
     protected function parseconf()
     {
-        $apps = opendir($this->root_path . 'apps');
+		// Loading the main configuration file first so we can get the paths.
+		$conf = array();
+		require('conf.php');
+		$this->apps_path = $conf['apps_path'];
+		$this->apps = $conf['apps'];
+
+		// Is this a relative path?
+		if(!preg_match('/^[A-Z]:/i', $this->apps_path)
+		   && $this->apps_path[0] != '/') {
+			$this->apps_path = $this->root_path . '/' . $this->apps_path;
+		}
+
+		// Alright. Now let's load the apps config.
+		$this->routes = array();
+		$this->app_routes = array();
+		foreach($this->apps as $app) {
+			$path = $this->apps_path . '/' . $app;
+			if(!file_exists($path) || !is_dir($path)) {
+				continue;
+			}
+			$app = array();
+			@include($path . '/bootstrap.php');
+			$routes[$app] = $app['route'];
+			foreach($app['route'] as $route => $callback) {
+				$this->app_routes[$route] = $app;
+			}
+		}
+
+		krsort($this->app_routes);
     }
+
+	/**
+	 * Serves requests
+	 */
+	public function serve()
+	{
+		$server = new atlatl\Server($_SERVER);
+		$route_to_app = "";
+
+		$method_routes = preg_grep('%^' . $server->getMethod() . ':%',
+								   $this->app_routes);
+		
+		foreach($this->app_routes as $route => $app) {
+			if(preg_match('%^'. $route .'%i', $server->getMethod() . ':' . $server->getRoute())) {
+				$route_to_app = $app;
+				break;
+			}
+		}
+
+		// Trying generic.
+		if(!$route_to_app) {
+			foreach($this->app_routes as $route => $app) {
+				if(preg_match('%^' . $route . '%i', $server->getRoute())) {
+					$route_to_app = $app;
+					break;
+				}
+			}
+		}
+
+		if(!$app) {
+			throw new Exception('Not found');
+		}
+
+		$app = new Atlatl\Core('', $server);
+		$app->serve($this->routes[$app]);
+	}
 }
 
 ?>

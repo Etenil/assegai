da845f89679f19df6daadf800f112be827f4ea1b
Guillaume Pasquet <guillaume.pasquet@metropolis.co.uk>
2012-06-11 17:55:23 +0100

    Added example for ACL.

--- lib/modules/acl/acl.php
+++ lib/modules/acl/acl.php
@@ -1,5 +1,36 @@
 <?php
 
+/**
+ * ACL module for Assegai
+ *
+ * Configuration example:
+
+ $app['modules'] = array('acl');
+
+ $app['acl'] = array(
+    'roles' => array(
+        'guest' => null,
+        'user'  => array('guest'),
+        'admin' => array('guest', 'user'),
+        ),
+    'resources' => array(
+        'news'  => null,
+        'story' => array('news'),
+        ),
+    'privileges' => array(
+        'guest' => array(
+            array('news', 'view'),
+            ),
+        'user' => array(
+            array('news', array('edit', 'publish')),
+            ),
+        'admin' => array(
+            array('news', array('view', 'edit', 'publish', 'delete')),
+            array('story', array('view', 'edit', 'publish', 'delete')),
+            ),
+        ),
+    );
+*/
 
 class Module_Acl extends \assegai\Module
 {

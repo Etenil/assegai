4b3691a3ef47ed52bd31e358c992900b70878331
etenil <boss@etenil.net>
2012-09-04 17:37:28 +0100

    Improved build script.

--- build.sh
+++ build.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
-# Keeping mercurial clone up to date.
+# Keeping atlatl's mercurial clone up to date.
 if [ -d lib/atlatl ]
 then
-    hg pull lib/atlatl/
-    hg up lib/atlatl/
+    echo "Updating atlatl... "
+    cd lib/atlatl
+    hg -q pull
+    hg -q up
+    cd ../../
+    echo "DONE"
 else
+    echo "Downloading atlatl... "
     curl -s http://atlatl.etenil.net/latest.gz | gunzip - > lib/atlatl.php
+    echo "DONE"
+fi
+
+# And cloning Centrifuge (for unit tests)
+if [ -d lib/centrifuge ]
+then
+    echo "Updating centrifuge... "
+    cd lib/centrifuge
+    hg -q pull 
+    hg -q up 
+    cd ../../
+    echo "DONE"
+else
+    echo "Downloading centrifuge... "
+    hg -q clone http://pikacode.com/etenil/centrifuge lib/centrifuge
+    echo "DONE"
 fi
 

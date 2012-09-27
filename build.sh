9002cc13db5401f3435ef17b0b9bfa6caf424b4c
Guillaume Pasquet <guillaume.pasquet@metropolis.co.uk>
2012-09-27 11:01:17 +0100

    Modified code to allow applications unit tests.

--- build.sh
+++ build.sh
@@ -14,19 +14,3 @@ else
     curl -s http://atlatl.etenil.net/latest.gz | gunzip - > lib/atlatl.php
     echo "DONE"
 fi
-
-# And cloning Centrifuge (for unit tests)
-if [ -d lib/centrifuge ]
-then
-    echo "Updating centrifuge... "
-    cd lib/centrifuge
-    hg -q pull 
-    hg -q up 
-    cd ../../
-    echo "DONE"
-else
-    echo "Downloading centrifuge... "
-    hg -q clone http://pikacode.com/etenil/centrifuge lib/centrifuge
-    echo "DONE"
-fi
-

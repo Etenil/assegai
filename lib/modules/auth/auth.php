49b5aa07db18e35bb34dc7b9a20f06412536e6e8
Guillaume Pasquet <guillaume@tutorfair.com>
2013-07-20 13:22:05 +0100

    Initial working auth module.

--- lib/modules/auth/auth.php
+++ lib/modules/auth/auth.php
@@ -1,12 +1,47 @@
 <?php
 
+require('interfaces.php');
+
 /**
  * Auth module for Assegai
  */
 class Module_Auth extends \assegai\Module
 {
+    protected $user_model;
+    protected $group_model;
+    protected $user_mapper;
+    protected $group_mapper;
+
+    protected $hash_method = 'sha256';
+
+    protected $users;
+    protected $groups;
+
+    public $user; // Currently logged-in user.
+
     public function _init($options)
     {
+        if(!isset($options['user']) || !isset($options['group'])) {
+            throw new \Exception('You must specify the User and Group models to use the Auth module.');
+        }
+
+        $this->user_model = $options['user'];
+        $this->group_model = $options['group'];
+
+        if(isset($options['usermapper'])) {
+            $this->user_mapper = $options['usermapper'];
+        } else {
+            $this->user_mapper = $this->user_model . 'Mapper';
+        }
+
+        if(isset($options['groupmapper'])) {
+            $this->group_mapper = $options['groupmapper'];
+        } else {
+            $this->group_mapper = $this->group_model . 'Mapper';
+        }
+
+        $this->users = new $this->user_mapper();
+        $this->groups = new $this->group_mapper();
     }
 
     public static function instanciate()
@@ -14,11 +49,53 @@ class Module_Auth extends \assegai\Module
 		return true;
 	}
 
-    public function authenticate($username, $password) {
-        // TODO Implement logic to check user.
+    protected function hash($data) {
+        if(function_exists('password_hash')) {
+            return password_hash($data, PASSWORD_DEFAULT);
+        } else {
+            return hash($this->hash_method, $data);
+        }
     }
 
-    public function login($username) {
+    protected function checkhash($data, $hash) {
+        if(function_exists('password_verify')) {
+            return password_verify($data, $hash);
+        } else {
+            return ($this->hash($data) == $hash);
+        }
+    }
+
+    public function register($username, $password, $email, $firstname = '', $lastname = '') {
+        $user = new $this->user_model();
+        $user->setUsername($username);
+        $user->setPasswordHash($this->hash($password));
+        $user->setEmail($email);
+        $user->setFirstname($firstname);
+        $user->setLastname($lastname);
+
+        $this->users->saveUser($user);
+
+        return $user;
+    }
+
+    public function authenticate($username, $password) {
+        $user = $this->users->loadUser($username);
+        if($this->checkhash($password, $user->getPasswordHash())) {
+            return $user;
+        } else {
+            return false;
+        }
+    }
+
+    public function login(\assegai\Request $request, $user) {
+        $request->setSession('auth_username', $user->getUsername());
+    }
+
+    public function preProcess($request, $proto) {
+        $username = $request->getSession('auth_username');
+        if($username) {
+            $this->user = $this->users->loadUser($username);
+        }
     }
 }
 
---
+++ lib/modules/auth/interfaces.php
@@ -0,0 +1,34 @@
+<?php
+
+interface IAuthUser {
+    function getUsername();
+    function setUsername($username);
+    function getPasswordHash();
+    function setPasswordHash($hash);
+    function getEmail();
+    function setEmail($email);
+    function getFirstname();
+    function setFirstname($firstname);
+    function getLastname();
+    function setLastname($lastname);
+    function getGroup();
+    function setGroup(IAuthGroup $group);
+}
+
+interface IAuthUserMapper {
+    function saveUser(IAuthUser $user);
+    function loadUser($username);
+    function dropUser(IAuthUser $user);
+}
+
+interface IAuthGroup {
+    function getName();
+    function setName($name);
+}
+
+interface IAuthGroupMapper {
+    function saveGroup(IAuthGroup $group);
+    function loadGroup($name);
+    function dropGroup(IAuthGroup $group);
+}
+

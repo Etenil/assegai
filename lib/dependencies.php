11a23095214b9a33eb1152e6f3e91237e0a81ba2
Guillaume Pasquet <guillaume@tutorfair.com>
2013-12-11 01:56:22 +0000

    Started moving to proper injection.

--- lib/dependencies.php
+++ lib/dependencies.php
@@ -3,36 +3,12 @@
 namespace assegai;
 
 // Core.
-Injector::register('Core', function($prefix = '') {
-        $server = Injector::give('Server', $_SERVER, $prefix);
-        $request = Injector::give('Request', $_GET, $_POST);
-        $mc = Injector::give('ModuleContainer', $server);
-        return new Core($prefix, $server, $request, $mc);
-    });
-
-// Server
-Injector::register('Server', function(array $data, $prefix = '') {
-        return new Server($data, $prefix);
-    });
-
+$injector->register('Core', array(
+    'Server', 'Request', 'ModuleContainer'
+));
+$injector->register('Server', array());
 // Request
-Injector::register('Request', function(array $get, array $post, array $session = null, array $cookies = null) {
-        return new Request($get, $post, $session ?: array(), $cookies ?: array());
-    });
-
-// ModuleContainer
-Injector::register('ModuleContainer', function(Server $server) {
-        return new ModuleContainer($server);
-    });
-
-// Response
-Injector::register('Response',
-    function($body = '', $status_code = 200,
-        $content_type = 'text/html; charset=UTF-8') {
-        return new Response($body, $status_code, $content_type);
-    });
-
-// Security
-Injector::register('Security', function() {
-        return new Security();
-    });
+$injector->register('Request', array());
+$injector->register('ModuleContainer', array('Server'));
+$injector->register('Response');
+$injector->register('Security');

219f2fc7970a5d88b86a67db79fb0608331f4898
Luis Ferro <luis.ferro@tutorfair.com>
2014-11-17 13:33:17 +0000

    allow for direct class definition if the class implements the required interface

--- src/assegai/modules/mail/Mail.php
+++ src/assegai/modules/mail/Mail.php
@@ -29,7 +29,12 @@ namespace assegai\modules\mail
             $classname = "\\assegai\\modules\\mail\\services\\Builtin";
             
             if(isset($options['service'])) { // We use the standard email service
-                $classname = "\\assegai\\modules\\mail\\services\\" . ucwords($options['service']);
+                if(class_exists($options['service']) && in_array( '\assegai\modules\mail\Service' ,class_implements($options['service']))) {
+                    $classname = $options['service'];
+                }
+                else {
+                    $classname = "\\assegai\\modules\\mail\\services\\" . ucwords($options['service']);
+                }
             }
 
             $this->svc = new $classname(@$options['options']);
